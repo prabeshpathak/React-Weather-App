a87ce522996eca68636d82aec12dcee20e2f4d44
Prabesh Pathak <prabeshpathak50@gmail.com>
2021-12-18 16:18:11 +0545

    Updates

---
+++ precache-manifest.6a9aff1c007f688f4543138199be6bd0.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "98e9c080200854232db7b2422bea7deb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e4e67cdd1a075c793a29",
+    "url": "/static/css/main.1cdc5372.chunk.css"
+  },
+  {
+    "revision": "9a7e5dd16e8ac5c0f11d",
+    "url": "/static/js/2.dfc00e7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.dfc00e7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4e67cdd1a075c793a29",
+    "url": "/static/js/main.d216fdc1.chunk.js"
+  },
+  {
+    "revision": "3fc8bf5ae6c824a13248",
+    "url": "/static/js/runtime-main.314f2c27.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
